b9edd915cfc50b6433679034731c77c366f29e0b
Raphael Nunes <raphaelm22@gmail.com>
2024-10-01 10:58:28 -0300

    feat: apdating to new Caesb and GrowthSupplements layouts

--- src/Crawling/Crawlers/Caesb/Handler.cs
+++ src/Crawling/Crawlers/Caesb/Handler.cs
@@ -15,66 +15,67 @@ namespace Crawling.Crawlers.Caesb
 
         public async Task<NotificationMessage?> Handle(Command request, CancellationToken cancellationToken)
         {
-            _logger.LogInformation("Starting crawling on Caesb to find {Neighborhood}", request.NeighborhoodName);
+            _logger.LogInformation("Starting crawling on Caesb to find {district}", request.DistrictName);
 
             await using var browser = await _puppeteerBrowserFactory.CreateAsync();
 
             using var page = await browser.NewPageAsync();
             await page.GoToAsync(URL, TimeSpan.FromSeconds(60).Milliseconds, [WaitUntilNavigation.DOMContentLoaded]);
+            await page.WaitForSelectorAsync("form[id='tabView:formFaltaDeAgua']", new() { Timeout = 30_000, Visible = true });
 
-            var neighborhoodInfo = await TryGetNeighborhoodInfo(page, request.NeighborhoodName);
-            if (neighborhoodInfo.Count > 0)
+            var districtInfo = await TryGetDistrictInfo(page, request.DistrictName);
+            if (districtInfo.Count > 0)
             {
-                return CreateNotification(neighborhoodInfo);
+                return CreateNotification(districtInfo);
             }
 
             return null;
         }
 
-        async Task<List<string>> TryGetNeighborhoodInfo(IPage page, string neighborhoodName)
+        async Task<List<string>> TryGetDistrictInfo(IPage page, string districtName)
         {
             try
             {
-                await page.WaitForSelectorAsync("form#formFaltaDeAgua table tbody tr");
+                await page.WaitForSelectorAsync("form[id='tabView:formFaltaDeAgua'] table tbody tr");
 
-                var neighborhoodElement = (await page.EvaluateExpressionAsync<IEnumerable<string>>(
+                var districtElement = (await page.EvaluateExpressionAsync<IEnumerable<string>>(
                     $"""
                     [...(
                         Array.from(document.querySelectorAll('td'))
-                            .find(el => el.textContent === '{neighborhoodName}')?.parentElement?.getElementsByTagName("td") || []
+                            .find(el => el.textContent === '{districtName}')?.parentElement?.getElementsByTagName("td") || []
                         )
                     ].map(el => el.innerText) 
                     """
                 ))
                 .ToList();
 
-                if (neighborhoodElement.Count == 0)
+                if (districtElement.Count == 0)
                 {
-                    _logger.LogError("Nothing about {neighborhood} was found.", neighborhoodName);
+                    _logger.LogError("Nothing about {district} was found.", districtName);
                 }
 
-                return neighborhoodElement;
+                return districtElement;
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "An error occurred while tried to find '{neighborhoodName}'.", neighborhoodName);
+                _logger.LogError(ex, "An error occurred while tried to find '{districtName}'.", districtName);
                 return [];
             }
         }
 
-        NotificationMessage? CreateNotification(List<string> neighborhoodInfo)
+        NotificationMessage? CreateNotification(List<string> districtInfo)
         {
-            if (neighborhoodInfo.Count == 6)
+            if (districtInfo.Count == 6)
             {
-                var message = $"Lack of water in: '{neighborhoodInfo[0]}'. \n\n" +
-                    $"Area: '{neighborhoodInfo[1]}'. \n\n" +
-                    $"Between: '{neighborhoodInfo[2]}' and '{neighborhoodInfo[3]}'. \n\n" +
-                    $"Reason: '{neighborhoodInfo[4]} - {neighborhoodInfo[5]}'";
+                var message = $"Lack of water in: '{districtInfo[0]}'. \n\n" +
+                    $"Area: '{districtInfo[1]}'. \n\n" +
+                    $"Between: '{districtInfo[2]}' and '{districtInfo[3]}'. \n\n" +
+                    $"Reason: '{districtInfo[4]} - {districtInfo[5]}'";
 
                 return new NotificationMessage(message);
             }
 
-            return new NotificationMessage(string.Join("\n", neighborhoodInfo));
+            return new NotificationMessage(string.Join("\n", districtInfo));
         }
     }
 }
