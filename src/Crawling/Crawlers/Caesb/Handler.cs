0a3ff0dda6facbfa082a98e2b26d83de228b5304
Raphael Nunes <raphaelm22@users.noreply.github.com>
2025-05-30 20:53:22 -0300

    fix: use include instead of equals to find district (#6)

--- src/Crawling/Crawlers/Caesb/Handler.cs
+++ src/Crawling/Crawlers/Caesb/Handler.cs
@@ -42,7 +42,7 @@ namespace Crawling.Crawlers.Caesb
                     $"""
                     [...(
                         Array.from(document.querySelectorAll('td'))
-                            .find(el => el.textContent === '{districtName}')?.parentElement?.getElementsByTagName("td") || []
+                            .find(el => el.textContent.toLowerCase().includes('{districtName}'.toLowerCase()))?.parentElement?.getElementsByTagName("td") || []
                         )
                     ].map(el => el.innerText) 
                     """
